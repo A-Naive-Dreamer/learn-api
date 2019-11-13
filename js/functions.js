514c25360150a1386e2dca2b490b234c7fa6ae89
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-11-13 16:20:42 +0700

    Add btn-group class

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 let gitHubAPI = angular.module('gitHubAPI', [])
 
-gitHubAPI.controller('gitHubAPIController', function ($scope, $http) {
+gitHubAPI.controller('gitHubAPIController', function ($scope, $http, $sce) {
     $scope.prepareGitHubAPI = () => {
         $http.get('https://api.github.com/users/A-Naive-Dreamer')
             .then(function (response1) {
@@ -11,14 +11,17 @@ gitHubAPI.controller('gitHubAPIController', function ($scope, $http) {
                         $scope.followers = response2.data
                         $scope.followerData = []
 
-                        console.log($scope.followers)
+                        // console.log($scope.followers)
 
                         // for (let x = 0; x < $scope.followers.length; ++x) {
-                        //     $http.get({
-                        //         method: 'POST',
-                        //         url: $scope.followers[x].html_url,
+                        //     let url = $scope.followers[x].html_url,
+                        //         trustedURL = $sce.trustAsResourceUrl(url)
+
+                        //     $http({
+                        //         method: 'JSONP',
+                        //         url: trustedURL,
                         //         headers: {
-                        //             'Access-Control-Allow-Origin': '*'
+                        //             'Cross-Origin-Allow-Control': '*'
                         //         }
                         //     })
                         //         .then(function (response3) {
@@ -37,11 +40,22 @@ gitHubAPI.controller('gitHubAPIController', function ($scope, $http) {
                     .then(function (response3) {
                         $scope.following = response3.data
                     })
+
+                $http.get($scope.userData.repos_url)
+                    .then(function (response4) {
+                        $scope.repos = response4.data
+                        console.log($scope.repos)
+                    })
             })
     }
 
     $scope.showFollowing = function () {
         $('#followers').css('display', 'none')
         $('#following').css('display', 'block')
+    }
+
+    $scope.showFollower = function () {
+        $('#followers').css('display', 'block')
+        $('#following').css('display', 'none')
     }
 })
