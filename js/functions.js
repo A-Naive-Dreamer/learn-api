39bd0ddbfd28ecb072da9fb4833200305e531119
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-12-07 22:39:56 +0700

    Made web page to be responsive.

--- js/functions.js
+++ js/functions.js
@@ -1,46 +1,27 @@
 let gitHubAPI = angular.module('gitHubAPI', [])
 
-gitHubAPI.controller('gitHubAPIController', function ($scope, $http, $sce) {
+gitHubAPI.controller('gitHubAPIController', function ($scope, $http) {
     $scope.prepareGitHubAPI = () => {
-        $http.get('https://api.github.com/users/A-Naive-Dreamer')
+        $http
+            .get('https://api.github.com/users/A-Naive-Dreamer')
             .then(function (response1) {
                 $scope.userData = response1.data
 
-                $http.get($scope.userData.followers_url)
+                $http
+                    .get($scope.userData.followers_url)
                     .then(function (response2) {
                         $scope.followers = response2.data
                         $scope.followerData = []
-
-                        // for (let x = 0; x < $scope.followers.length; ++x) {
-                        //     let url = $scope.followers[x].html_url,
-                        //         trustedURL = $sce.trustAsResourceUrl(url)
-
-                        //     $http({
-                        //         method: 'JSONP',
-                        //         url: trustedURL,
-                        //         headers: {
-                        //             'Cross-Origin-Allow-Control': 'https://api.github.com'
-                        //         }
-                        //     })
-                        //         .then(function (response3) {
-                        //             $scope.followerData.push(response3.data)
-                        //             console.log($scope.followerData)
-                        //         })
-                        // }
-                        // $scope.followers.forEach(function (follower) {
-                        //     $http.get(follower.html_url)
-                        //         .then(function (response3) {
-                        //             $scope.followerData.push(response3)
-                        //         })
-                        // })
                     })
 
-                $http.get($scope.userData.url + '/following')
+                $http
+                    .get($scope.userData.url + '/following')
                     .then(function (response3) {
                         $scope.following = response3.data
                     })
 
-                $http.get($scope.userData.repos_url)
+                $http
+                    .get($scope.userData.repos_url)
                     .then(function (response4) {
                         $scope.repos = response4.data
                     })
@@ -77,8 +58,14 @@ gitHubAPI.controller('gitHubAPIController', function ($scope, $http, $sce) {
 
     $scope.search = function () {
         $('#follower-result').empty()
+
         $scope.followers.forEach(function (follower) {
-            if ($scope.searchKeywords != '' && follower.login.toLowerCase().includes($scope.searchKeywords)) {
+            if (
+                $scope.searchKeywords !== '' &&
+                follower.login
+                    .toLowerCase()
+                    .includes($scope.searchKeywords)
+            ) {
                 let followerResult = document.getElementById('follower-result'),
                     li = document.createElement('li'),
                     div1 = document.createElement('div'),
@@ -108,9 +95,16 @@ gitHubAPI.controller('gitHubAPIController', function ($scope, $http, $sce) {
                 h3.append(follower.login)
             }
         })
+
         $('#following-result').empty()
+
         $scope.following.forEach(function (following) {
-            if ($scope.searchKeywords != '' && following.login.toLowerCase().includes($scope.searchKeywords)) {
+            if (
+                $scope.searchKeywords !== '' &&
+                following.login
+                    .toLowerCase()
+                    .includes($scope.searchKeywords)
+            ) {
                 let followingResult = document.getElementById('following-result'),
                     li = document.createElement('li'),
                     div1 = document.createElement('div'),
